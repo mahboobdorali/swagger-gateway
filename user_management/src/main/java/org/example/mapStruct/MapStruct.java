ab41aaaec9f8dcf36a2a289173cbd48d1e44c3af
mahboobdorali <mahboobdorali@gmail.com>
2024-04-01 15:56:56 +0330

    add mapstruct interface for use mapper

---
+++ user_management/src/main/java/org/example/mapStruct/MapStruct.java
@@ -0,0 +1,18 @@
+package org.example.mapStruct;
+
+import org.example.dto.UserRequestDto;
+import org.example.dto.UserResponseDto;
+import org.example.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface MapStruct {
+
+    MapStruct INSTANCE = Mappers.getMapper(MapStruct.class);
+
+    User UserResponseDtoToModel(UserResponseDto userResponseDto);
+    User UserRequestDtoToModel(UserRequestDto userRequestDto);
+    UserResponseDto modelToUserResponseDto(User user);
+//    List<User> dtoListToModelList(List<UserDto> userDtoList);
+}
